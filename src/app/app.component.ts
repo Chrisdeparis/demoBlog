e99abec70d759cdd18aac99d1a6e1d2877d3c588
maescristro <milfortchristian@gmail.com>
2018-04-17 13:53:59 +0200

    modif blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  
+
+  post: {
+    title: string,
+    content: string,
+    loveIts: boolean,
+    created_at: Date
+  } 
+  
+  loveIts = 0;
+  postStatus = this.loveIts;
+  console.log(this.loveIts);
+  posts = [
+    {
+      title: "Mon premier post",
+      content: 'content post 1'
+      
+    },
+    {
+      title: "Mon deuxieme post",
+      content: 'content post 2'
+    },
+    {
+      title: "Mon troisieme post",
+      content: 'content post 3'
+    },
+  ];
+
+  onLoveIt(){
+    console.log(this.loveIts = this.loveIts + 1);
+  }
+  dontLoveIt(){
+    console.log(this.loveIts = this.loveIts -1);
+  }
+  // postName = 'titre du post';
+
+
+  getStatus() {
+    return this.loveIts;
+  }
+
+  getColor() {
+    if( this.loveIts +1 > 0) {
+      return 'green';
+    } else if(this.loveIts +1 <= 0) {
+      return 'red';
+    }
+  }
+
+  
+  
+  
+
+  
+
 }
